ef256c48548ae30967c149c1af444b71a37f76a2
isabelpt <56853689+isabelpt@users.noreply.github.com>
2020-08-06 17:29:36 -0400

    Add files via upload

---
+++ MiniProject2/MiniProject2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniProject2
+//
+//  Created by Isabel Prado-Tucker on 8/6/20.
+//  Copyright © 2020 Isabel Prado-Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
